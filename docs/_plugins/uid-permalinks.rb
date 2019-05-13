676f22dc95e11cba0c9ef72eb303e2eb7d70575a
Will Cavanagh <will.cavanagh@autodesk.com>
2019-05-12 18:14:00 -0700

    Clean up PermalinkRewriter generator.

--- docs/_plugins/uid-permalinks.rb
+++ docs/_plugins/uid-permalinks.rb
@@ -15,12 +15,18 @@ module Jekyll
 
     class PermalinkRewriter < Jekyll::Generator
         safe true
-        priority :low
+        # set priority to highest, since we're rewriting page URLs and other
+        # plugins depend on these URLs during generation.
+        priority :highest
 
         def generate(site)
+            # We'll keep track of the consumed hashes so we can detect a
+            # collision.
             consumed_hashes = []
             site.pages.each do |item|
                 if item.data["pagename"]
+                    # We'll generate UID urls for all pages with a pagename
+                    # field in their slug.
                     pagename = item.data["pagename"].to_s
                     if pagename == "index"
                         # Special caae for root index -- we want it to sit at /,
@@ -31,12 +37,19 @@ module Jekyll
                         # Generate a hash from the pagename and truncate to 8
                         # characters for better usability.
                         uid = Digest::SHA1.hexdigest(pagename)[0..7]
-                        # If hashes collide, raise an exception
+                        # If hashes collide, raise an exception.
+                        # The likelyhook of a collision with SHA1 truncated to
+                        # 8 chars is very small...  If it becomes a problem we
+                        # could do something here to deal with it, however it
+                        # wouldn't be as simple as say shifting the windpow on
+                        # the has, since existing URIs can't change when a new
+                        # page is added that collides.
                         if consumed_hashes.include? uid
                             raise PageUIDCollisionException.new
                         end
                         consumed_hashes << uid
-                        # copy the site config permalink to modify
+                        # Copy the site config permalink to use on this page,
+                        # and replace the UID key with the generated UID.
                         url = site.config["permalink"].dup
                         url = url.gsub! ":uid", uid
                         item.data["permalink"] = url
